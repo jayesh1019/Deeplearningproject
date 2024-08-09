0acf959b13965d0464d76f6094d524306d13de20
jayesh1019 <jayesh1323@gmail.com>
2024-08-09 14:31:58 +0530

    training config updated

--- Xray/components/data_ingestion.py
+++ Xray/components/data_ingestion.py
@@ -8,11 +8,25 @@ from Xray.logger import logging
 
 
 class DataIngestion:
-    def __init__(self):
-        pass
+    def __init__(self, data_ingestion_config: DataIngestionConfig):
+        self.data_ingestion_config = data_ingestion_config
+        
+        self.s3=S3Operation()
+
+        
     def get_data_from_s3(self):
         try:
-            pass
+            logging.info("Entered the get_data_from_s3 method of Data Ingestion class")
+
+            self.s3.sync_folder_from_s3(
+                folder=self.data_ingestion_config.data_path,
+                bucket_name=self.data_ingestion_config.bucket_name,
+                bucket_folder_name=self.data_ingestion_config.S3_data_folder
+
+            )
+
+            logging.info("Exited the get_data_from_s3 method of Data Ingestion class")
+
         except Exception as e:
             raise XRayException(e,sys)
     
